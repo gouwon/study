d00ca8cbe5001bbda74590c4edb1b74b64e3719f
고원 <44748728+gouwon@users.noreply.github.com>
2023-08-31 11:19:46 +0900

    Fix: 개인 정보 수정 및 로그인 버튼 클릭 활성화 수정
    Fix: 개인 정보 수정 및 로그인 버튼 클릭 활성화 수정
    
    개인 정보값 수정
    로그인 버튼 클릭 주석 처리 부분 삭제

--- project/001.Auto_Commute/prt-auto_commute.js
+++ project/001.Auto_Commute/prt-auto_commute.js
@@ -2,12 +2,12 @@
 /* 인터넷 브라우저 개발자 도구 > 콘솔에서 실행 */
 
 /* 설정 */
-var user_id = 'gwkim';
-var user_pw = 'ahsgjs&3g';
-var user_nm = '김건우';
-var telegram = {'token': '6038387779:AAHGGLhhKcIxCN2BStwr_1FxKcSqa0hgnNc',
-                'name': 'nu_1_bot',
-                'chatId': ''
+var user_id = '{그룹웨어 사용자 ID}';
+var user_pw = '{그룹웨어 사용자 PW}';
+var user_nm = '{그룹웨어 사용자 이름}';
+var telegram = {'token': '{Telegram TOKEN값}',
+                'name': '{Telegram Bot 이름}',
+                'chatId': '{Telegram Bot Chat ID}'
 }
 var tlgr_msg_dict = {'success': '출근 등록하였습니다.',
                 'fail': '출근 등록하지 못 했습니다.'
@@ -37,7 +37,7 @@ console.log('1>>>> ', time_interval_to_execute)
 setTimeout(function(isOff) {
     if (isOff) {
         if ($('#disIN').textContent === '출근입력') {
-            /* $('#disIN').click(); */
+            $('#disIN').click();
             console.log('2>>>> ', time_interval_to_execute);
         }
     }
