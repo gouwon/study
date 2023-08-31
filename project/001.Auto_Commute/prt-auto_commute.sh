4dc0009374619fb8f26779e980f62c1c624b8738
고원 <44748728+gouwon@users.noreply.github.com>
2023-08-31 11:10:27 +0900

    KT Bizmeka 자동 출근 입력
    Feat: KT Bizmeka Auto Commute Python Crontab registered Shell Script

---
+++ project/001.Auto_Commute/prt-auto_commute.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+export DISPLAY=:0 && /home/gouwon/venv/py_venv3.9/bin/python /home/gouwon/development/02.Project/001.Auto_Commute/prt-auto_commute.py prt-auto_commute.toml > /home/gouwon/auto_commute.log 2>&1
